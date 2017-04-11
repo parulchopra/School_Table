b04ee63f4e7ad1c64e0b18976e09fb6cb03d46ea
parulchopra <parul.chopra53@gmail.com>
2017-04-11 22:28:49 +0530

    recgular shaper added

--- app/src/main/java/com/example/parulchopra/kids_table/MainActivity.java
+++ app/src/main/java/com/example/parulchopra/kids_table/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.parulchopra.kids_table;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 EditText E1;
+    TextView T1;
     Button B1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        T1 =(TextView)findViewById(R.id.text);
+        Typeface face= Typeface.createFromAsset(MainActivity.this.getAssets(), "fonts/FFF_Tusj.ttf");
+        T1.setTypeface(face);
         E1=(EditText)findViewById(R.id.edit);
         B1=(Button)findViewById(R.id.button);
+
+        Typeface face1= Typeface.createFromAsset(MainActivity.this.getAssets(), "fonts/Sail-Regular.otf");
+        E1.setTypeface(face1);
+        B1.setTypeface(face);
         B1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/parulchopra/kids_table/TableShow_Activity.java
+++ app/src/main/java/com/example/parulchopra/kids_table/TableShow_Activity.java
@@ -1,8 +1,13 @@
 package com.example.parulchopra.kids_table;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -20,11 +25,12 @@ public class TableShow_Activity extends AppCompatActivity {
         LinearLayout layout = (LinearLayout) findViewById(R.id.Activity2);
 
 
+
         for (int i = 1; i < 31; i++) {
             LinearLayout row = new LinearLayout(this);
 
-            row.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
+            row.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            row.setGravity(Gravity.CENTER_HORIZONTAL);
             for (int j = 1; j <2; j++) {
                 int t = Integer.parseInt(value);
                String k = String.valueOf(i * t);
@@ -35,28 +41,39 @@ public class TableShow_Activity extends AppCompatActivity {
                 TextView T3 = new TextView(getApplication());
                 TextView T4 = new TextView(getApplication());
                 TextView T5 = new TextView(getApplication());
-
+                Typeface face1= Typeface.createFromAsset(TableShow_Activity.this.getAssets(), "fonts/PlayfairDisplaySC-BoldItalic.otf");
+                T1.setTypeface(face1);
+                T2.setTypeface(face1);
+                T3.setTypeface(face1);
+                T4.setTypeface(face1);
+                T5.setTypeface(face1);
 
                 row.addView(T1);
                 row.addView(T2);
                 row.addView(T3);
                 row.addView(T5);
                 row.addView(T4);
-                T1.setTextColor(android.graphics.Color.argb(255, 26, 26, 26));
-                T1.setTextSize(40);
-                T2.setTextColor(android.graphics.Color.argb(255, 26, 26, 26));
-                T2.setTextSize(40);
-                T3.setTextColor(android.graphics.Color.argb(255, 26, 26, 26));
-                T3.setTextSize(40);
-                T4.setTextColor(android.graphics.Color.argb(255, 26, 26, 26));
-                T4.setTextSize(40);
-                T5.setTextColor(android.graphics.Color.argb(255, 26, 26, 26));
-                T5.setTextSize(40);
+
+                T1.setTextSize(50);
+                T2.setTextSize(50);
+                T3.setTextSize(50);
+                T4.setTextSize(50);
+                T5.setTextSize(50);
+
+                T1.setTextColor(Color.parseColor("#FF41309F"));
+                T2.setTextColor(Color.parseColor("#FF41309F"));
+                T3.setTextColor(Color.parseColor("#FF41309F"));
+                T4.setTextColor(Color.parseColor("#FF41309F"));
+                T5.setTextColor(Color.parseColor("#FF41309F"));
+
+
+
                 T1.setText(value);
                 T5.setText(" = ");
                 T2.setText(" * ");
                 T3.setText(String.valueOf(i));
                 T4.setText(String.valueOf(k + "\n"));
+
             }
 
             layout.addView(row);
